332ef9e899b52fa7272544e4753eb4a555a9f3f7
Stiicer <diegoandremora@gmail.com>
2022-09-28 13:27:08 -0400

    comments

--- assets/script.js
+++ assets/script.js
@@ -2,32 +2,25 @@ var APIkey = '975449a451msh2a06483e508d496p18c10djsnf0466bf81871';
 var ing = document.getElementById('Hungryfor');
 
 //API for food search
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '975449a451msh2a06483e508d496p18c10djsnf0466bf81871',
-        'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
-    }
-};
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': '975449a451msh2a06483e508d496p18c10djsnf0466bf81871',
+//         'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+//     }
+// };
 
-fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=' + ing.value, options)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+// fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=' + ing.value, options)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.error(err));
  
  
-    //API for movie Search
-    const options1 = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-            'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://ott-details.p.rapidapi.com/advancedsearch?start_year='+1970+'&end_year='+2020+'&min_imdb='+6+'&max_imdb='+7.8+'&genre='+action+'&language=english&type='+movie+'&sort=latest&page=any', options1)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+
+    // // fetch('https://ott-details.p.rapidapi.com/advancedsearch?start_year='+1970+'&end_year='+2020+'&min_imdb='+6+'&max_imdb='+7.8+'&genre='+action+'&language=english&type='+movie+'&sort=latest&page=any', options1)
+    // //     .then(response => response.json())
+    // //     .then(response => console.log(response))
+    // //     .catch(err => console.error(err));
 
 
+  
